e2dc3c4d677b7ad9d24b0370751cefc4477e203d
ty61271 <40565834+ty61271@users.noreply.github.com>
2018-10-16 23:27:04 +0800

    firstCommit

--- app/src/main/java/com/expamle/love/travelstory/MainActivity.java
+++ app/src/main/java/com/expamle/love/travelstory/MainActivity.java
@@ -1,52 +1,36 @@
 package com.expamle.love.travelstory;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText("首頁");
-                    return true;
-                case R.id.navigation_recommend:
-                    mTextMessage.setText("推薦");
-                    return true;
-                case R.id.navigation_search:
-                    mTextMessage.setText("搜尋");
-                    return true;
-                case R.id.navigation_like:
-                    mTextMessage.setText("收藏");
-                    return true;
-                case R.id.navigation_user:
-                    mTextMessage.setText("用戶");
-                    return true;
-
-            }
-            return false;
-        }
-    };
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
     }
+//    fragment
+    class FramgentAdater extends FragmentStatePagerAdapter {
 
+        public FramgentAdater(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+    }
 }
