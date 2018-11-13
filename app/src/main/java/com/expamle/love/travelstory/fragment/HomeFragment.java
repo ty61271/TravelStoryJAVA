28d0b23e1ed09d23832415a2443d2dec919fae54
ty61271 <40565834+ty61271@users.noreply.github.com>
2018-11-13 23:52:42 +0800

    1113 first change2

--- app/src/main/java/com/expamle/love/travelstory/fragment/HomeFragment.java
+++ app/src/main/java/com/expamle/love/travelstory/fragment/HomeFragment.java
@@ -52,78 +52,78 @@ public class HomeFragment extends Fragment {
 
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        findViews();
-//      list資料
-        hotCities = getCityList();
-        HomeAdater adater = new HomeAdater();
-        recyclerView.setAdapter(adater);
-
-    }
-
-
-
-    //      綁定
-    private void findViews() {
-        recyclerView = getActivity().findViewById(R.id.recycler_hot_city);
-        recyclerView.setHasFixedSize(false);
-        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
-    }
-
-    //      Adater監聽器
-    class HomeAdater extends RecyclerView.Adapter<HomeAdater.HomeHolder> {
-
-        @NonNull
-        @Override
-        public HomeHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-            View view = getLayoutInflater().inflate(R.layout.item_icon, viewGroup, false);
-            return new HomeHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull HomeHolder homeHolder, int i) {
-            HotCity hotCity = hotCities.get(i);
-            homeHolder.hotCityName.setText(hotCity.getCityName());
-            homeHolder.hotCityView.setImageResource(hotCity.getCityPicture());
-            homeHolder.hotCityView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    startActivity(new Intent(getActivity(), CityActivity.class));
-                }
-            });
-        }
-
-        @Override
-        public int getItemCount() {
-            return hotCities.size();
-        }
-
-        class HomeHolder extends RecyclerView.ViewHolder {
-            TextView hotCityName;
-            ImageView hotCityView;
-
-            public HomeHolder(@NonNull View itemView) {
-                super(itemView);
-                hotCityName = itemView.findViewById(R.id.city_name);
-                hotCityView = itemView.findViewById(R.id.city_icon);
-            }
-        }
-
-    }
-    public List<HotCity> getCityList(){
-        hotCities=new ArrayList<>();
-        hotCities.add(new HotCity("台灣", R.drawable.taipei));
-        hotCities.add(new HotCity("日本", R.drawable.okinawa));
-        hotCities.add(new HotCity("日本", R.drawable.okinawa));
-        hotCities.add(new HotCity("日本", R.drawable.okinawa));
-        hotCities.add(new HotCity("日本", R.drawable.okinawa));
-        hotCities.add(new HotCity("日本", R.drawable.okinawa));
-        hotCities.add(new HotCity("日本", R.drawable.okinawa));
-        hotCities.add(new HotCity("日本", R.drawable.okinawa));
-        hotCities.add(new HotCity("日本", R.drawable.okinawa));
-        return hotCities;
-    }
+//    @Override
+//    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+//        super.onActivityCreated(savedInstanceState);
+//        findViews();
+////      list資料
+//        hotCities = getCityList();
+//        HomeAdater adater = new HomeAdater();
+//        recyclerView.setAdapter(adater);
+//
+//    }
+//
+//
+//
+//    //      綁定
+//    private void findViews() {
+//        recyclerView = getActivity().findViewById(R.id.recycler_hot_city);
+//        recyclerView.setHasFixedSize(false);
+//        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+//    }
+//
+//    //      Adater監聽器
+//    class HomeAdater extends RecyclerView.Adapter<HomeAdater.HomeHolder> {
+//
+//        @NonNull
+//        @Override
+//        public HomeHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+//            View view = getLayoutInflater().inflate(R.layout.item_icon, viewGroup, false);
+//            return new HomeHolder(view);
+//        }
+//
+//        @Override
+//        public void onBindViewHolder(@NonNull HomeHolder homeHolder, int i) {
+//            HotCity hotCity = hotCities.get(i);
+//            homeHolder.hotCityName.setText(hotCity.getCityName());
+//            homeHolder.hotCityView.setImageResource(hotCity.getCityPicture());
+//            homeHolder.hotCityView.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    startActivity(new Intent(getActivity(), CityActivity.class));
+//                }
+//            });
+//        }
+//
+//        @Override
+//        public int getItemCount() {
+//            return hotCities.size();
+//        }
+//
+//        class HomeHolder extends RecyclerView.ViewHolder {
+//            TextView hotCityName;
+//            ImageView hotCityView;
+//
+//            public HomeHolder(@NonNull View itemView) {
+//                super(itemView);
+//                hotCityName = itemView.findViewById(R.id.city_name);
+//                hotCityView = itemView.findViewById(R.id.city_icon);
+//            }
+//        }
+//
+//    }
+//    public List<HotCity> getCityList(){
+//        hotCities=new ArrayList<>();
+//        hotCities.add(new HotCity("台灣", R.drawable.taipei));
+//        hotCities.add(new HotCity("日本", R.drawable.okinawa));
+//        hotCities.add(new HotCity("日本", R.drawable.okinawa));
+//        hotCities.add(new HotCity("日本", R.drawable.okinawa));
+//        hotCities.add(new HotCity("日本", R.drawable.okinawa));
+//        hotCities.add(new HotCity("日本", R.drawable.okinawa));
+//        hotCities.add(new HotCity("日本", R.drawable.okinawa));
+//        hotCities.add(new HotCity("日本", R.drawable.okinawa));
+//        hotCities.add(new HotCity("日本", R.drawable.okinawa));
+//        return hotCities;
+//    }
 
 }
